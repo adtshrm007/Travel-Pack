6df1406f4f994cb2c5c37de4e01faaa7b844a31a
Aditya <adtshrm1@gmail.com>
2025-02-23 23:25:48 +0530

    Initail Commit

--- Components/SignUp.jsx
+++ Components/SignUp.jsx
@@ -8,35 +8,44 @@ export default function SignUp() {
           <source src={SignUpVideo} type="video/mp4" />
           Your browser does not support the video tag.
         </video>
-        <div className="header">
-          <div className="logo">
-            <p>
-              <i class="fa-solid fa-earth-americas"></i> Travel Pack
-            </p>
+        <div className="xyz">
+          <div className="signup-form">
+            <p className="abc">Sign Up</p>
+            <div className="signup-mainform">
+              <div className="emailid">
+                <input type="text" placeholder="Email Id"></input>
+              </div>
+              <div className="emailid">
+                <input type="text" placeholder="Username"></input>
+              </div>
+              <div className="emailid">
+                <input type="password" placeholder="Password"></input>
+              </div>
+              <div className="emailid">
+                <input type="password" placeholder="Confirm Password"></input>
+              </div>
+
+              <div className="button">
+                <button>Sign Up</button>
+              </div>
+              <p className="next">
+                Have an account?<Link to="/login">Login</Link>
+              </p>
+            </div>
           </div>
-        </div>
-        <div className="signup-form">
-          <p className="abc">Sign Up</p>
-          <div className="signup-mainform">
-            <div className="emailid">
-              <input type="text" placeholder="Email Id"></input>
+          <div className="google">
+            <div className="google1">
+              <i class="fa-brands fa-google"></i>
+              <p>Sign Up using Google</p>
             </div>
-            <div className="emailid">
-              <input type="text" placeholder="Username"></input>
+            <div className="google1">
+            <i class="fa-brands fa-facebook"></i>
+            <p>Sign Up using Facebook</p>
             </div>
-            <div className="emailid">
-              <input type="password" placeholder="Password"></input>
+            <div className="google1">
+            <i class="fa-solid fa-phone"></i>
+            <p>Sign Up using Phone Number</p>
             </div>
-            <div className="emailid">
-              <input type="password" placeholder="Confirm Password"></input>
-            </div>
-            
-            <div className="button">
-              <button>Sign Up</button>
-            </div>
-            <p className="next">
-              Don't have an account?<Link to="/signup">Sign up</Link>
-            </p>
           </div>
         </div>
       </div>
